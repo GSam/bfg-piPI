a0a0ec8c334f14d31c63f5af26b82bbd2879e6c3
GSam <garming_sam@hotmail.com>
2017-11-14 21:01:40 +1300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-virtualenv
+wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.9.0-linux64.tar.bz2
+tar xvf pypy2-v5.9.0-linux64.tar.bz2
+virtualenv -p pypy2-v5.9.0-linux64/bin/pypy pypy-env
+cd pypy-env
+source bin/activate
+pip install z3-solver
+pip install rpython
