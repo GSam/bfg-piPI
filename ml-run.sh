1de3a1618244d29264ad34f6b636f4d68ec836b4
GSam <garming_sam@hotmail.com>
2017-11-17 01:03:26 +1300

    Update ml-run.sh

--- ml-run.sh
+++ ml-run.sh
@@ -1,4 +1,11 @@
 sudo apt-get install libopenblas-dev
 pip install numpy
+pip install scipy
 
 # pandas, scipy, ...
+
+# natural language
+pip install nltk
+python -c "import nltk; nltk.download('brown')"
+
+pip install gensim
