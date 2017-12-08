7f4b6a5f5c2d844eb301ef2b45268adae6bad5cc
GSam <garming_sam@hotmail.com>
2017-12-08 16:02:26 +1300

    Update stdlib.py

--- stdlib.py
+++ stdlib.py
@@ -1,15 +1,15 @@
-def rffi_run:
+def rffi_run():
     from rpython.rtyper.lltypesystem import rffi, lltype
     from rpython.translator.tool.cbuild import ExternalCompilationInfo
     external_function = rffi.llexternal('myprint', [], lltype.Void, compilation_info=ExternalCompilationInfo(libraries=['testlib.so']))
     external_function()
     
-def ctypes_run:
+def ctypes_run():
     import ctypes
     testlib = ctypes.CDLL('testlib.so')
     testlib.myprint()
 
-def cffi_run:
+def cffi_run():
     ffi = FFI()
     ffi.cdef("""void myprint(void);""")
     testlib = ffi.dlopen('/home/garming/pypy-env/testlib.so')
